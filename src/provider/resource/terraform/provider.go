dd9015614bfe589a88c9f4f950c78837415d3c8e
TORU IKEDA <t-ikeda@sios.com>
2023-11-02 17:09:10 +0900

    Remove unnecessary ctx

--- src/provider/resource/terraform/provider.go
+++ src/provider/resource/terraform/provider.go
@@ -1,18 +1,16 @@
 package terraform
 
 import (
-	"context"
 	"fmt"
 	"os"
 	"os/exec"
 	"path/filepath"
+
 	"github.com/hashicorp/hcl/v2/hclwrite"
 	"github.com/zclconf/go-cty/cty"
 )
 
 type TerraformResourceProvider struct{}
-
-var ctx = context.Background()
 
 func (p TerraformResourceProvider) Create(parameters map[string]interface{}) (map[string]interface{}, error) {
 	f := hclwrite.NewEmptyFile()
@@ -59,19 +57,19 @@ func (p TerraformResourceProvider) Create(parameters map[string]interface{}) (ma
 	if err := os.MkdirAll(dir, 0755); err != nil {
 		return nil, fmt.Errorf("failed to create directories: %w", err)
 	}
-	
+
 	// Write the file
 	if err := os.WriteFile(filePath, f.Bytes(), 0644); err != nil {
 		return nil, fmt.Errorf("failed to write file: %w", err)
 	}
-	
+
 	fmt.Printf("HCL written to %s\n", filePath)
-	
+
 	// Terraform exec
 	if err := p.ApplyTerraform(filePath); err != nil {
 		return nil, fmt.Errorf("failed to apply terraform: %w", err)
 	}
-	
+
 	return nil, nil
 }
 
@@ -99,7 +97,7 @@ func writeHCL(body *hclwrite.Body, key string, value interface{}) {
 func (TerraformResourceProvider) ApplyTerraform(filePath string) error {
 	// Terraform init
 	dir := filepath.Dir(filePath)
-	
+
 	initCmd := exec.Command("terraform", "init")
 	initCmd.Dir = dir
 	if output, err := initCmd.CombinedOutput(); err != nil {
