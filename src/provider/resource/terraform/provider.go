166b18767bdbef041302b5d1ab452cd7a6671c66
kanta sasaki <ka-sasaki@sios.com>
2023-11-22 16:12:14 +0900

    tf json exec

--- src/provider/resource/terraform/provider.go
+++ src/provider/resource/terraform/provider.go
@@ -2,15 +2,12 @@ package terraform
 
 import (
 	"dacrane/pdk"
-	"dacrane/utils"
 	"encoding/json"
 	"fmt"
 	"os"
 	"os/exec"
 	"path/filepath"
-
-	"github.com/hashicorp/hcl/v2/hclwrite"
-	"github.com/zclconf/go-cty/cty"
+	"github.com/hashicorp/hcl/v2/hclparse"
 )
 
 var TerraformResource = pdk.Resource{
@@ -40,113 +37,53 @@ var TerraformResource = pdk.Resource{
 
 func Create(parameter any, meta pdk.ProviderMeta) (any, error) {
 	parameters := parameter.(map[string]any)
-	f := hclwrite.NewEmptyFile()
-	rootBody := f.Body()
+	resourceName := parameters["resource"].(string)
+	argument := parameters["argument"].(map[string]any)
 
-	// Setting Provider
-	providerName, ok := parameters["provider"].(string)
-	if !ok {
-		return nil, fmt.Errorf("provider name is required and must be a string")
-	}
-	providerBlock := rootBody.AppendNewBlock("provider", []string{providerName})
-	providerBody := providerBlock.Body()
-	if configs, ok := parameters["configurations"].(map[string]interface{}); ok {
-		for k, v := range configs {
-			writeHCL(providerBody, k, v)
-		}
+	mainTf := map[string]any{
+		"resource": map[string]any{
+			resourceName: map[string]any{
+				"main": argument,
+			},
+		},
 	}
 
-	// Setting Resource
-	resourceType, ok := parameters["resource"].(string)
-	if !ok {
-		return nil, fmt.Errorf("resource type is required and must be a string")
-	}
-	resourceName := "main"
-	resourceBlock := rootBody.AppendNewBlock("resource", []string{resourceType, resourceName})
-	resourceBody := resourceBlock.Body()
-	if args, ok := parameters["argument"].(map[string]interface{}); ok {
-		for k, v := range args {
-			writeHCL(resourceBody, k, v)
-		}
+	byteData, err := json.Marshal(mainTf)
+	if err != nil {
+		fmt.Println("Error marshalling to JSON:", err)
+		return nil, nil
 	}
 
-	// write file
-	filename := "main.tf"
-	dir := meta.CustomStateDir
+	parser := hclparse.NewParser()
+
+	// Parse the JSON string to obtain an HCL file object.
+	_, diags := parser.ParseJSON(byteData, "config0.json")
+
+	if diags.HasErrors() {
+		diags.Errs()
+		return nil, nil
+	}
+
+	if diags.HasErrors() {
+		fmt.Println(diags.Errs())
+		return nil, nil 
+	}
+	if err != nil {
+		fmt.Println("Error saving HCL file:", err)
+	}
+
+	dir := ".dacrane/instances/my-quick-start/"
+	filename := "config.tf.json"
 	filePath := filepath.Join(dir, filename)
-
-	// Ensure the directory exists
-	if err := os.MkdirAll(dir, 0755); err != nil {
-		return nil, fmt.Errorf("failed to create directories: %w", err)
+	if err := os.WriteFile(filePath, byteData, 0644); err != nil {
+		return nil, fmt.Errorf("Error writing JSON file: %v", err)
 	}
 
-	// Write the file
-	if err := os.WriteFile(filePath, f.Bytes(), 0644); err != nil {
-		return nil, fmt.Errorf("failed to write file: %w", err)
-	}
-
-	fmt.Printf("HCL written to %s\n", filePath)
-
-	// Terraform exec
 	if err := ApplyTerraform(filePath); err != nil {
-		return nil, fmt.Errorf("failed to apply terraform: %w", err)
-	}
-
-	// Get Terraform State
-	bytes, err := os.ReadFile(dir + "/terraform.tfstate")
-	if err != nil {
 		return nil, err
 	}
 
-	var state map[string]any
-	err = json.Unmarshal(bytes, &state)
-	if err != nil {
-		return nil, err
-	}
-
-	resource := utils.Find(state["resources"].([]any), func(r any) bool {
-		return r.(map[string]any)["mode"] == "managed" &&
-			r.(map[string]any)["type"] == resourceType &&
-			r.(map[string]any)["name"] == resourceName
-	})
-
-	instances := resource.(map[string]any)["instances"]
-	instance := instances.([]any)[0]
-	attributes := instance.(map[string]any)["attributes"]
-	return attributes.(map[string]any), nil
-}
-
-func writeHCL(body *hclwrite.Body, key string, value interface{}) {
-	switch v := value.(type) {
-	case map[string]interface{}:
-		isMap, ok := v["$is_map"]
-		if ok && isMap.(bool) {
-			delete(v, "$is_map")
-			vs := map[string]cty.Value{}
-			for k, val := range v {
-				vs[k] = cty.StringVal(val.(string))
-			}
-			body.SetAttributeValue(key, cty.MapVal(vs))
-		} else {
-			block := body.AppendNewBlock(key, nil)
-			blockBody := block.Body()
-			for k, val := range v {
-				writeHCL(blockBody, k, val)
-			}
-		}
-	case string:
-		body.SetAttributeValue(key, cty.StringVal(v))
-	case bool:
-		body.SetAttributeValue(key, cty.BoolVal(v))
-	case []interface{}:
-		values := make([]cty.Value, len(v))
-		for i, val := range v {
-			values[i] = cty.StringVal(val.(string))
-		}
-		body.SetAttributeValue(key, cty.ListVal(values))
-	default:
-		fmt.Printf("Unsupported type: %T\n", v)
-	}
+	return nil, nil
 }
 
 func ApplyTerraform(filePath string) error {
